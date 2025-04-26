ed332f0df60628e700796ae1c52589d33fbf27a1
Ladas Nikolaos <ladas.s.nikolaos@gmail.com>
2025-04-26 18:11:52 +0300

    Original commit

---
+++ src/main/java/com/example/TraineeshipManager/TraineeshipManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TraineeshipManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeshipManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeshipManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TraineeshipManager/TraineeshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TraineeshipManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
